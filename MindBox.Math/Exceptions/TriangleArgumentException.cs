22d1802bbe036de8839cea9506b3e29255bfb8d4
EfgraF505 <egor.leskov.03@mail.ru>
2023-02-06 01:57:12 +0300

    Добавьте файлы проекта.

---
+++ MindBox.Math/Exceptions/TriangleArgumentException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindBox.Geometry.Exceptions
+{
+    internal class TriangleArgumentException : Exception
+    {
+        public TriangleArgumentException() 
+            : base("The triangle is rectangular") { }
+    }
+}
