2cd890bd2846daf3a4bdfd857c2492ecbb6ea65a
Egraf505 <72596818+Egraf505@users.noreply.github.com>
2023-02-07 02:14:55 +0300

    Update SqlQuery.sql

--- SqlQuery.sql
+++ SqlQuery.sql
@@ -1,1 +1,5 @@
-﻿
+﻿SELECT product.title, category.title
+FROM product
+LEFT JOIN product_category ON product.id = product_category.product_id
+LEFT JOIN category ON category.id = product_category.category_id
+ORDER BY product.title
